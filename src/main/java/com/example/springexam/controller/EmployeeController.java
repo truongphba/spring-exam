c2e527fe6976da79d4e21e463643b3feff327365
truongphba <truongph.ba@gmail.com>
2021-06-02 20:10:15 +0700

    truongph

---
+++ src/main/java/com/example/springexam/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.example.springexam.controller;
+
+import com.example.springexam.entity.Employee;
+import com.example.springexam.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public Iterable<Employee> getList() {
+        return employeeRepository.findAll();
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public Employee createEmployee(Employee employee){
+        return employeeRepository.save(employee);
+    }
+}
